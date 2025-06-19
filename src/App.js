b70ec406e2bd6ad0098805ceae73baac38d11034
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-06-19 19:38:52 +0200

    created user form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import UserForm from './components/UserForm';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>User Height App</h1>
         <p>Welcome! Please enter your details below.</p>
+        <UserForm />
       </header>
     </div>
   );
