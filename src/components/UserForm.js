fe2ef4fa741b1a655043f07834c36508fef53c0c
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-06-19 19:38:33 +0200

    created user form

---
+++ src/components/UserForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UserForm = () => {
+  return (
+    <form>
+      <label>Name:</label>
+      <input type="text" name="name" />
+
+      <label>Height (in cm):</label>
+      <input type="number" name="height" />
+
+      <label>Email:</label>
+      <input type="email" name="email" />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UserForm;
