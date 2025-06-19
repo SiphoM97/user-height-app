bed239413306b6e5c5bf9e0a5dc524ca0c91598c
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-06-19 20:09:57 +0200

    form updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import UserForm from './components/UserForm';
 function App() {
   return (
+    
     <div className="App">
+      <title>User height App</title>
       <header className="App-header">
         <h1>User Height App</h1>
         <p>Welcome! Please enter your details below.</p>
-        <UserForm />
+        <UserForm/>
       </header>
     </div>
   );
