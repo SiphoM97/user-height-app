fe3387f46c82405955a2db670bce3a3e726cb2fa
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-06-21 08:24:15 +0200

    added useState and handleChange.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,14 +1,49 @@
+import React, { useState } from 'react';
+
 const UserForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    height: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Name:</label>
-      <input type="text" name="name" />
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
 
       <label>Height (in cm):</label>
-      <input type="number" name="height" />
+      <input
+        type="number"
+        name="height"
+        value={formData.height}
+        onChange={handleChange}
+      />
 
       <label>Email:</label>
-      <input type="email" name="email" />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
 
       <button type="submit">Submit</button>
     </form>
